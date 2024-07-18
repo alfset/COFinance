2b5bc7d75cba4c86de01ef887cf912f4d1ec7d13
Alf <42708476+alfset@users.noreply.github.com>
2024-07-18 21:07:47 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', 
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+      },
+    },
+  },
+  server: {
+    port: 3000, 
+    open: true,
+  },
 });
